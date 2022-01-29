dec24010f5868b227376871118055c12d27bfb51
Abhilash Upadhyay <93393330+AbhilashUpadhyay@users.noreply.github.com>
2022-01-30 02:13:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+ "use strict"
+let goUp= document.getElementById("goUp").addEventListener("click",function(){
+    window.scrollTo(0,0);
+    
+})
+
+let home= document.getElementById("home").addEventListener("click",function(){
+    window.scrollTo(0,0);
+})
+let about= document.getElementById("about").addEventListener("click",function(){
+    window.scrollTo(0,700);
+})
+let services = document.getElementById("services").addEventListener("click",function(){
+    window.scrollTo(0,1900);
+})
+let contact= document.getElementById("contact").addEventListener("click",function(){
+    window.scrollTo(0,2500);
+})
